17e34b84d2b31255e160b286111b9759b51e875f
Sakib <saadmansakib275@gmail.com>
2024-04-25 03:47:27 +0600

    practice

---
+++ app/Http/Controllers/DeliveryManController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Delivery_Man;
+use App\Http\Requests\StoreDelivery_ManRequest;
+use App\Http\Requests\UpdateDelivery_ManRequest;
+
+class DeliveryManController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDelivery_ManRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Delivery_Man $delivery_Man)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Delivery_Man $delivery_Man)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDelivery_ManRequest $request, Delivery_Man $delivery_Man)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Delivery_Man $delivery_Man)
+    {
+        //
+    }
+}
